4f21c84b80e6a8639e2434ed1e0201382c305cd7
semrithiro <rithiro@gmail.com>
2017-07-06 12:06:36 +0700

    update

--- Shell Script/Test.sh
+++ Shell Script/Test.sh
@@ -1,2 +1,13 @@
-
-echo "Hello World"
+#!/bin/bash/
+set result = $RANDOM;
+TOP:
+echo "Please input value: "; read value
+if [ $value -eq $result ] then
+	echo "You got the correct answer"
+elseif [ $value -gt $result ] then
+	echo "Smaller than this"
+	goto TOP
+else
+	echo "Bigger than this"
+	goto TOP
+fi
